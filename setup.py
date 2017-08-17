5de5cf81468a6243067d61fede1617ae2668c86e
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-08-17 15:39:39 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-camera-imagefield',
+    version='0.1',
+    description="A Django widget for using a device's camera to get an image to upload",
+    author='Alexander Dutton',
+    author_email='code@alexdutton.co.uk',
+    url='https://github.com/alexsdutton/django-camera-imagefield',
+    packages=find_packages(),
+    install_requires=['django', 'python-datauri']
+)
